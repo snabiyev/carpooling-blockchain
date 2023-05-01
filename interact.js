ee13ac0957d40d888caaeed2253e1ec36318a547
snabiyev <snabiyev12104@ada.edu.az>
2023-05-01 21:38:05 +0400

    clean commit

--- interact.js
+++ interact.js
@@ -1,5 +1,4 @@
-const contract = require("./build/contracts/PaymentProcessingService.json");
-const contract_address = "0x84b7C55A57824979620a1CCb0CFF589155408599"
+const contract = require("./build/contracts/VehicleManagementService.json");
 
 const abi = contract.abi;
 console.log(JSON.stringify(abi))
